28afe540f472eea5b2fb7f320520b121836e724d
Daniil <losev.megapolys@yandex.ru>
2023-03-26 14:02:45 +0300

    3) Футер - check

--- templates/scripts.js
+++
@@ -1,6 +0,0 @@
-function activeLinkControl() {
-    $('.navbar-nav .nav-link a').click(function () {
-        $('.nav-link').removeClass('active')
-        $(this).closest('.nav-link').addClass('active')
-    })
-}
