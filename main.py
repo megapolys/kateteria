92b781917b4554bd8d480c75a4df058bde329827
Vane86 <budar12@yandex.ru>
2023-04-15 14:15:02 +0300

    release 1.0.2

--- main.py
+++ main.py
@@ -250,4 +250,5 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'img/favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
